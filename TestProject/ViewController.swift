22e2dcf748ee653caac86ec4f41d5f6871eede72
Matvey Fedyshin <mnf89@mail.ru>
2024-01-12 18:35:19 +0300

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Матвей Федышин on 12.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
